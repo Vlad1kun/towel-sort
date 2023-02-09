fb85ec5a5dfb3e4eb1a244819e39a8094f6b1d92
Vlad1kun <viparrow@mail.ru>
2023-02-09 22:22:10 +0300

    init: algoritm_work

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let masComp = []
+  for (i in matrix) {
+    if (i % 2 == 1) {
+      masComp.push(matrix[i].reverse())
+    } else {
+      masComp.push(matrix[i])
+    }
+  }
+  return masComp;
 }
