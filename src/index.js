9f01470b2ef18e9e534dd5833b173de5e051839c
Vlad1kun <viparrow@mail.ru>
2023-02-09 22:24:42 +0300

    init: work_in_the_end

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ module.exports = function towelSort (matrix) {
   let masComp = []
   for (i in matrix) {
     if (i % 2 == 1) {
-      masComp.push(matrix[i].reverse())
+      masComp.push(...matrix[i].reverse())
     } else {
-      masComp.push(matrix[i])
+      masComp.push(...matrix[i])
     }
   }
   return masComp;
